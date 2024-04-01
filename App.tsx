5df059d4819c82e0c9795ef087ec5844bd8e931b
Tymchenko Vladyslav <vlad.timchenko@gmail.com>
2024-04-01 05:54:36 +0200

    First COmmit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require('./assets/logo.png')}
+        />
+      </View>
+      <View style={styles.btnsContainer}>
+        <View style={styles.button}>
+          <Button title='Connecter' />
+        </View>
+        <View style={styles.button}>
+          <Button title="S'inscrire" />
+        </View>
+        <View style={styles.button}>
+          <Button title='Deconnecter' />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +28,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logoContainer: {
+    flex: 5,
+  },
+  logo: {
+    resizeMode: 'center',
+    // flex:1
+  },
+  btnsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    height: 10,
+  },
+
+  button: {
+    flex: 1,
+
+    // marginHorizontal:5
   },
 });
